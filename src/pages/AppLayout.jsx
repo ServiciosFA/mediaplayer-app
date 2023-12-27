5b736caefb54fb71d36859543801f5ca8dc28ad6
Acosta Fernando <104600953+ServiciosFA@users.noreply.github.com>
2023-12-27 16:35:45 -0300

    Update AppLayout.jsx

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import NavBar from "../ui/NavBar";
 import "./AppLayout.scss";
@@ -21,27 +21,3 @@ const AppLayout = () => {
 };
 
 export default AppLayout;
-=======
-import React from "react";
-import NavBar from "../ui/NavBar";
-import "./AppLayout.scss";
-import { Outlet } from "react-router-dom";
-import LocationInfo from "../hook/useToken";
-import RefreshToken from "../hook/useRefreshToken";
-
-const AppLayout = () => {
-  return (
-    <div className="layout">
-      {/*Hook useToken*/}
-      <LocationInfo></LocationInfo>
-      <RefreshToken></RefreshToken>
-      <NavBar></NavBar>
-      <div className="pagesContainer">
-        <Outlet></Outlet>
-      </div>
-    </div>
-  );
-};
-
-export default AppLayout;
->>>>>>> 805ca04de9f2543c83cf36a9994dc34056352e95
